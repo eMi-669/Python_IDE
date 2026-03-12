5f4203c560238df7c2a57df4df9bf5c195a7ba4e
Gatiev Mukhammad <emiema279@gmail.com>
2026-03-12 09:55:46 +0300

    Добавлен текстовый редактор для написания python кода

--- main.py
+++ main.py
@@ -9,6 +9,19 @@ class PythonIDE:
         self.root = root
         self.root.title("Python IDE")
 
+        frame = tk.Frame(self.root)
+        frame.pack(fill=tk.BOTH, expand=True)
+
+        self.editor = scrolledtext.ScrolledText(
+            frame,
+            wrap=tk.WORD,
+            font=("Courier New", 12),
+            height=20,
+            undo=True
+        )
+
+        self.editor.pack(fill=tk.BOTH, expand=True)
+
 if __name__ == "__main__":
     root = tk.Tk()
     ide = PythonIDE(root)
