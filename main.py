ca3ded10561799a77fdd84eb66985b8c4dca28e7
Gatiev Mukhammad <emiema279@gmail.com>
2026-03-12 17:45:54 +0300

    Добавлено меню работы с файлами

--- main.py
+++ main.py
@@ -9,6 +9,30 @@ class PythonIDE:
         self.root = root
         self.root.title("Python IDE")
 
+        frame = tk.Frame(self.root)
+        frame.pack(fill=tk.BOTH, expand=True)
+
+        self.editor = scrolledtext.ScrolledText(
+            frame,
+            wrap=tk.WORD,
+            font=("Courier New", 12),
+            height=20,
+            undo=True
+        )
+
+        self.editor.pack(fill=tk.BOTH, expand=True)
+
+        menu = tk.Menu(self.root)
+        self.root.config(menu=menu)
+
+        file_menu = tk.Menu(menu, tearoff=0)
+        menu.add_cascade(label="Файл", menu=file_menu)
+
+        file_menu.add_command(label="Открыть")
+        file_menu.add_command(label="Сохранить")
+        file_menu.add_separator()
+        file_menu.add_command(label="Выход", command=self.root.quit)
+
 if __name__ == "__main__":
     root = tk.Tk()
     ide = PythonIDE(root)
